0cabcf10d61a613c65744670d3f3f2dde0f81ca1
Natalia <natib13@gmail.com>
2022-03-22 00:49:04 +0100

    change in py

--- run_c.py
+++ run_c.py
@@ -1,13 +1,15 @@
-import os
+import subprocess
 
 loops = [1000, 10**4, 10**5, 10**6, 10**7, 10**8, 10**9, 10**10, 10**11]
 msg_size = [x for x in range(1, 3000, 100)]
 result_file = "results_1.txt"
 
 compile_c = "mpicc -o send_rec send_recv.c"
-run_c = "mpiexec -machinefile ./allnodes -np 2 ./send_rec "
+run_c = "mpiexec -machinefile ./allnodes -np 2 ./send_rec"
 
-os.system(compile_c)
+subprocess.run(compile_c)
 for size in msg_size:
     for loop in loops:
-        os.system(run_c + f"{loop} {size} {result_file}")
+        subprocess.run(f"{run_c} {loop} {size} {result_file}")
+
+        
