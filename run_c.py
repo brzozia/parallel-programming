da6e8955eb94894eeb3100fec89ce50272fd1250
brzozows <brzozows@vnode-03.dydaktyka.icsr.agh.edu.pl>
2022-03-22 12:38:03 +0100

    2_1 done

--- run_c.py
+++ run_c.py
@@ -2,7 +2,7 @@ import subprocess
 
 loops = [x for x in range(1000, 10**4, 1000) ]
 msg_size = [x for x in range(1, 3000, 100)]
-result_file = "./results_1_2.txt"
+result_file = "./results_1_2n.txt"
 
 compile_c = "mpicc -o send_rec send_recv.c"
 run_c = "mpiexec -machinefile ./allnodes -np 2 ./send_rec "
@@ -10,6 +10,6 @@ run_c = "mpiexec -machinefile ./allnodes -np 2 ./send_rec "
 subprocess.Popen(compile_c.split(" "))
 for size in msg_size:
     for loop in loops:
-        proces = subprocess.Popen((run_c + str(loop) + " " + str(size) + " " + str(result_file)).split(" "))
+        proces = subprocess.Popen((run_c + str(loop) + " " + str(size) + " " + str(result_file)+ " b " ).split(" "))
         proces.wait()
         print(proces.stdout)
