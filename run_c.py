807ece39b84a39da7885ddd62642bc0793491f94
brzozows <brzozows@vnode-08.dydaktyka.icsr.agh.edu.pl>
2022-03-22 13:26:52 +0100

    results

--- run_c.py
+++ run_c.py
@@ -2,7 +2,7 @@ import subprocess
 
 loops = [x for x in range(1000, 10**4, 1000) ]
 msg_size = [x for x in range(1, 3000, 100)]
-result_file = "./results_1_2n.txt"
+result_file = "./results_1_2_2n.txt"
 
 compile_c = "mpicc -o send_rec send_recv.c"
 run_c = "mpiexec -machinefile ./allnodes -np 2 ./send_rec "
