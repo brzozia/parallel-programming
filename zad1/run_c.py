9035c900235fe2de5862d4f94abbd9f54a3b7569
Natalia <natib13@gmail.com>
2022-03-23 22:12:30 +0100

    change

--- zad1/run_c.py
+++ zad1/run_c.py
@@ -11,7 +11,7 @@ run_c = "mpiexec -machinefile ./allnodes2 -np 2 ./send_rec "
 subprocess.Popen(compile_c.split(" "))
 # for ai in range(1,3):
 for loop in loops:
-        for size in msg_size:
-            proces = subprocess.Popen((run_c + str(loop) + " " + str(size) + " " + str(result_file)+" aa").split(" "))
-            proces.wait()
-            print(proces.stdout)
+    for size in msg_size:
+        proces = subprocess.Popen((run_c + str(loop) + " " + str(size) + " " + str(result_file)+" aa").split(" "))
+        proces.wait()
+        print(proces.stdout)
