411c5826ee334563629aabe5178dae3fa6878f00
Natalia <natib13@gmail.com>
2022-03-23 21:55:58 +0100

    comm printfs

--- zad1/run_c.py
+++ zad1/run_c.py
@@ -1,15 +1,17 @@
 import subprocess
 
-loops = [x for x in range(1000, 10**4, 1000) ]
-msg_size = [x for x in range(1, 3000, 100)]
-result_file = "./results_1_2.txt"
+loops = [10000]
+# x for x in range(1000, 10**4, 1000)
+msg_size = [x for x in range(1, 30000, 1000)]
+result_file = "./results_fin_"
 
 compile_c = "mpicc -o send_rec send_recv.c"
 run_c = "mpiexec -machinefile ./allnodes -np 2 ./send_rec "
 
 subprocess.Popen(compile_c.split(" "))
-for size in msg_size:
+for ai in range(1,3):
     for loop in loops:
-        proces = subprocess.Popen((run_c + str(loop) + " " + str(size) + " " + str(result_file)).split(" "))
-        proces.wait()
-        print(proces.stdout)
+        for size in msg_size:
+            proces = subprocess.Popen((run_c + str(loop) + " " + str(size) + " " + str(result_file)+str(ai)+"txt").split(" "))
+            proces.wait()
+            print(proces.stdout)
