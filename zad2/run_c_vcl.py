3dba5f4047341a3f1b39cfad86d29a6c283d0365
Natalia <natib13@gmail.com>
2022-03-23 11:33:31 +0100

    change allnodes in py

--- zad2/run_c_vcl.py
+++ zad2/run_c_vcl.py
@@ -13,7 +13,7 @@ compile_c = "mpicc -o pi paralel_pi_lab2.c"
 subprocess.Popen(compile_c.split(" "))
 for points_no in points:
     for proc in procesors:
-        run_c = "mpiexec -machinefile ./allnodes -np " + str(proc) + " ./pi " + str(points_no) + " " + str(result_file)
+        run_c = "mpiexec -machinefile ./allnodes" +str(proc)+ " -np " + str(proc) + " ./pi " + str(points_no) + " " + str(result_file)
         proces = subprocess.Popen(run_c.split(" "))
         proces.wait()
         print(proces.stdout)
