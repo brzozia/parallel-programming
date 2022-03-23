1d5236b711393b5b78352a52109da5ce52604114
brzozows <brzozows@vnode-08.dydaktyka.icsr.agh.edu.pl>
2022-03-23 22:09:42 +0100

    1 res

--- zad1/run_c.py
+++ zad1/run_c.py
@@ -7,12 +7,12 @@ msg_size = [x for x in range(1, 30000, 1000)]
 result_file = "./results_fin_"
 
 compile_c = "mpicc -o send_rec send_recv.c"
-run_c = "mpiexec -machinefile ./allnodes -np 2 ./send_rec "
+run_c = "mpiexec -machinefile ./allnodes1 -np 2 ./send_rec "
 
 subprocess.Popen(compile_c.split(" "))
-for ai in range(1,3):
-    for loop in loops:
-        for size in msg_size:
-            proces = subprocess.Popen((run_c + str(loop) + " " + str(size) + " " + str(result_file)+str(ai)+"txt").split(" "))
-            proces.wait()
-            print(proces.stdout)
+#for ai in range(1,3):
+for loop in loops:
+    for size in msg_size:
+        proces = subprocess.Popen((run_c + str(loop) + " " + str(size) + " " + str(result_file)+str(1)+".txt aaa").split(" "))
+        proces.wait()
+        print(proces.stdout)
