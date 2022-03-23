16771c2cfc14258f47008f13e5ea9cbc4bf9d713
Natalia <natib13@gmail.com>
2022-03-23 22:09:25 +0100

    1_89

--- zad1/run_c.py
+++ zad1/run_c.py
@@ -3,15 +3,15 @@ import subprocess
 loops = [10000]
 # x for x in range(1000, 10**4, 1000)
 msg_size = [x for x in range(1, 30000, 1000)]
-result_file = "./results_fin_"
+result_file = "./results_fin1_89.txt"
 
 compile_c = "mpicc -o send_rec send_recv.c"
-run_c = "mpiexec -machinefile ./allnodes -np 2 ./send_rec "
+run_c = "mpiexec -machinefile ./allnodes2 -np 2 ./send_rec "
 
 subprocess.Popen(compile_c.split(" "))
-for ai in range(1,3):
-    for loop in loops:
+# for ai in range(1,3):
+for loop in loops:
         for size in msg_size:
-            proces = subprocess.Popen((run_c + str(loop) + " " + str(size) + " " + str(result_file)+str(ai)+"txt").split(" "))
+            proces = subprocess.Popen((run_c + str(loop) + " " + str(size) + " " + str(result_file)+" aa").split(" "))
             proces.wait()
             print(proces.stdout)
