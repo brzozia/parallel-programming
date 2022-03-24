dde26084498bf01449a93ef11185e8d06184bafd
Natalia <natib13@gmail.com>
2022-03-24 09:53:37 +0100

    c

--- zad2/plot.py
+++ zad2/plot.py
@@ -6,6 +6,7 @@ from plotly.subplots import make_subplots
 
 df1 = pd.read_csv('results_promet_v19.txt', ";")
 df2 = pd.read_csv('results_promet_v20.txt', ";")
+
 df3 = pd.read_csv('results_promet_v14.txt', ";")
 df4 = pd.read_csv('results_promet_v15.txt', ";")
 df5 = pd.read_csv('results_promet_v16.txt', ";")
@@ -17,6 +18,12 @@ df9 = pd.read_csv('results_promet_v10.txt', ";")
 df10 = pd.read_csv('results_promet_v11.txt', ";")
 df11 = pd.read_csv('results_promet_v12.txt', ";")
 df12 = pd.read_csv('results_promet_v13.txt', ";")
+df13 = pd.read_csv('results_promet_v21.txt', ";")
+df14 = pd.read_csv('results_promet_v22.txt', ";")
+df15 = pd.read_csv('results_promet_v23.txt', ";")
+df16 = pd.read_csv('results_promet_v24.txt', ";")
+df17 = pd.read_csv('results_promet_v25.txt', ";")
+df18 = pd.read_csv('results_promet_v26.txt', ";")
 
 df1.columns = ['proc', 'all_points', 'my_points', 'inside_points', 'start', 'parall', 'end', 'pi', "none"]
 df2.columns = ['proc', 'all_points', 'my_points', 'inside_points', 'start', 'parall', 'end', 'pi', "none"]
@@ -30,8 +37,14 @@ df9.columns = ['proc', 'all_points', 'my_points', 'inside_points', 'start', 'par
 df10.columns = ['proc', 'all_points', 'my_points', 'inside_points', 'start', 'parall', 'end', 'pi', "none"]
 df11.columns = ['proc', 'all_points', 'my_points', 'inside_points', 'start', 'parall', 'end', 'pi', "none"]
 df12.columns = ['proc', 'all_points', 'my_points', 'inside_points', 'start', 'parall', 'end', 'pi', "none"]
+df13.columns = ['proc', 'all_points', 'my_points', 'inside_points', 'start', 'parall', 'end', 'pi', "none"]
+df14.columns = ['proc', 'all_points', 'my_points', 'inside_points', 'start', 'parall', 'end', 'pi', "none"]
+df15.columns = ['proc', 'all_points', 'my_points', 'inside_points', 'start', 'parall', 'end', 'pi', "none"]
+df16.columns = ['proc', 'all_points', 'my_points', 'inside_points', 'start', 'parall', 'end', 'pi', "none"]
+df17.columns = ['proc', 'all_points', 'my_points', 'inside_points', 'start', 'parall', 'end', 'pi', "none"]
+df18.columns = ['proc', 'all_points', 'my_points', 'inside_points', 'start', 'parall', 'end', 'pi', "none"]
 
-df = [df1, df2, df3, df4, df5, df6,df7, df8, df9, df10, df11, df12]
+df = [df1, df2, df3, df4, df5, df6,df7, df8, df9, df10, df11, df12, df13, df14, df15, df16, df17, df18]
 
 all_df = {}
 
@@ -180,8 +193,21 @@ def speedup(fig, x1, yz1, yz2, yz3):
         x=x1,
         y=y3,
         mode='markers',
-        name="5*10^10",
+        name="5*10^9",
         marker_symbol='diamond',
+        marker=dict(
+            color=randint(1, 500),
+            line_width=2,
+            size=7,
+        ),
+    ))
+    fig.add_trace(go.Scatter(
+        x=x1,
+        y=x1,
+        mode='lines',
+        name="idealne przyspieszenie",
+        # marker_symbol='diamond',
+        fillcolor='yellow',
         marker=dict(
             color=randint(1, 500),
             line_width=2,
@@ -198,33 +224,39 @@ def speedup(fig, x1, yz1, yz2, yz3):
 
 
 
-def sekw(fig):
-    x1 = []
+def sekw(fig, x1, yz1, yz2, yz3):
+    x1 = x1[1:]
     y1 = []
     y2 = []
     y3 = []
+    for i in range(1, 13,1):
+        y1.append(((1/yz1[i]) - (1/(i+1)) ) / (1 - (1/(i+1))))
+        y2.append(((1/yz2[i]) - (1/(i+1)) ) / (1 - (1/(i+1))))
+        y3.append(((1/yz3[i]) - (1/(i+1)) ) / (1 - (1/(i+1))))
 
-    tp1 = -1
-    tp2 = -1
-    tp3 = -1
-    for key,d in all_df.items():
-        x1.append(key)
 
-        t1 = [x['end'] - x['start'] for i, x in d.iterrows() if x['all_points']==4000000]
-        t2 = [x['end'] - x['start'] for i, x in d.iterrows() if x['all_points']==300000000]
-        t3 = [x['end'] - x['start'] for i, x in d.iterrows() if x['all_points']==5000000000]
+
+    # tp1 = -1
+    # tp2 = -1
+    # tp3 = -1
+    # for key,d in all_df.items():
+    #     x1.append(key)
+
+    #     t1 = [x['end'] - x['start'] for i, x in d.iterrows() if x['all_points']==4000000]
+    #     t2 = [x['end'] - x['start'] for i, x in d.iterrows() if x['all_points']==300000000]
+    #     t3 = [x['end'] - x['start'] for i, x in d.iterrows() if x['all_points']==5000000000]
 
 
         
 
-        if(tp1==-1):
-            tp1 = sum(t1)/len(t1)
-            tp2 = sum(t2)/len(t2)
-            tp3 = sum(t3)/len(t3)
-        else:
-            y1.append(tp1/(sum(t1)/len(t1)))
-            y2.append(tp2/(sum(t2)/len(t2)))
-            y3.append(tp3/(sum(t3)/len(t3))) 
+    #     if(tp1==-1):
+    #         tp1 = sum(t1)/len(t1)
+    #         tp2 = sum(t2)/len(t2)
+    #         tp3 = sum(t3)/len(t3)
+    #     else:
+    #         y1.append(tp1/(sum(t1)/len(t1)))
+    #         y2.append(tp2/(sum(t2)/len(t2)))
+    #         y3.append(tp3/(sum(t3)/len(t3))) 
 
     
     fig.add_trace(go.Scatter(
@@ -241,6 +273,18 @@ def sekw(fig):
     ))
     fig.add_trace(go.Scatter(
         x=x1,
+        y=[0 for x in x1],
+        mode='lines',
+        name="idealna część sekwencyjna",
+        marker_symbol='diamond',
+        marker=dict(
+            color=randint(1, 500),
+            line_width=2,
+            size=7,
+        ),
+    ))
+    fig.add_trace(go.Scatter(
+        x=x1,
         y=y2,
         mode='markers',
         name="3*10^8",
@@ -255,7 +299,7 @@ def sekw(fig):
         x=x1,
         y=y3,
         mode='markers',
-        name=5*10^10,
+        name="5*10^9",
         marker_symbol='diamond',
         marker=dict(
             color=randint(1, 500),
@@ -264,18 +308,18 @@ def sekw(fig):
         ),
     ))
     fig.update_layout(
-        title="Przyspieszenie programu",
+        title="Część sekwencyjna programu",
         xaxis_title="Ilość procesorów",
-        yaxis_title="Przyspieszenie"
+        yaxis_title="Część sekwencyjna"
     )
 
     return x1, y1, y2, y3
 
 
 def efectivity(fig, x1, y1, y2, y3):
-    y1 = y1/x1
-    y2 = y2/x1
-    y3 = y3/x1
+    y1 = [y1 / x1 for y1,x1 in zip(y1, x1)]
+    y2 = [y2 / x1 for y2,x1 in zip(y2, x1)]
+    y3 = [y3 / x1 for y3,x1 in zip(y3, x1)]
 
     fig.add_trace(go.Scatter(
         x=x1,
@@ -284,7 +328,7 @@ def efectivity(fig, x1, y1, y2, y3):
         name="4*10^6",
         marker_symbol='diamond',
         marker=dict(
-            color=randint(1, 500),
+            color='orange',
             line_width=2,
             size=7,
         ),
@@ -303,12 +347,24 @@ def efectivity(fig, x1, y1, y2, y3):
     ))
     fig.add_trace(go.Scatter(
         x=x1,
-        y=y3,
-        mode='markers',
-        name=5*10^10,
+        y=[1 for x in x1],
+        mode='lines',
+        name="idealna efektywność",
         marker_symbol='diamond',
         marker=dict(
-            color=randint(1, 500),
+            color='orange',
+            line_width=2,
+            size=7,
+        ),
+    ))
+    fig.add_trace(go.Scatter(
+        x=x1,
+        y=y3,
+        mode='markers',
+        name="5*10^9",
+        marker_symbol='diamond',
+        marker=dict(
+            color='green',
             line_width=2,
             size=7,
         ),
@@ -331,7 +387,7 @@ fig4 = go.Figure()
 x1, y1, y2, y3 = time(fig)
 x1, y1, y2, y3 = speedup(fig2, x1, y1, y2, y3)
 efectivity(fig3, x1, y1, y2, y3)
-sekw(fig4)
+sekw(fig4, x1, y1, y2, y3)
 
 fig.show()
 fig2.show()
