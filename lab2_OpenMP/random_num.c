807e7a755c4816bf0caf67c86f6b1b1342950361
Natalia <natib13@gmail.com>
2022-03-31 13:53:08 +0200

    paralel v1

--- lab2_OpenMP/random_num.c
+++ lab2_OpenMP/random_num.c
@@ -5,10 +5,26 @@
 
 int main(int argc, char ** argv){
 
+    int size=16;
+    // if(argc>=2){
+    //     size = 10;
+    // }
+
+    int *numbers = (int*)malloc(size*sizeof(int));
+
+    double start = omp_get_wtime();
     #pragma omp parallel
     {
-        printf("test");
+        int pid = omp_get_thread_num();
+       for(int i=pid; i<size; i+=4){
+           numbers[i] = pid;
+       }
     }
+    double end = omp_get_wtime();
 
-
+    printf("time: %f", end-start);
+    for(int i=0;i<size;i++){
+        printf("%d ", numbers[i]);
+    }
+    free(numbers);
 }
