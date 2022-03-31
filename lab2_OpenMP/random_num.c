65398eb275a39c95af8d842b525b582f6878d0ba
Natalia <natib13@gmail.com>
2022-03-31 14:16:29 +0200

    works faster!

--- lab2_OpenMP/random_num.c
+++ lab2_OpenMP/random_num.c
@@ -5,31 +5,31 @@
 
 int main(int argc, char ** argv){
 
-    int size=16;
+    int size=1600000000000;
     // if(argc>=2){
     //     size = 10;
     // }
 
     int *numbers = (int*)malloc(size*sizeof(int));
     int i, pid, thr;
-    thr = omp_get_num_threads();
     
     double start = omp_get_wtime();
-    #pragma omp parallel shared(numbers, size, thr) private(i, pid)
+    #pragma omp parallel shared(numbers, size) private(i, pid, thr)
     {
+        thr = omp_get_num_threads();
         pid = omp_get_thread_num();
         i = 0;
        for(i=pid; i<size; i+=thr){
-           printf("my pid: %d", pid);
+           //printf("my pid: %d", pid);
            numbers[i] = pid;
        }
     }
     double end = omp_get_wtime();
 
     printf("time: %f \n", end-start);
-    int j;
-    for(j=0;j<size;j++){
-        printf("%d ", numbers[j]);
-    }
+    //int j;
+  //  for(j=0;j<size;j++){
+    //    printf("%d ", numbers[j]);
+   // }
     free(numbers);
 }
