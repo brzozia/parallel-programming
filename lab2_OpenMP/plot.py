4084ab012603cc1ee7a13a6ef362ea936dd2b957
Natalia <natib13@gmail.com>
2022-04-06 23:52:27 +0200

    changes

--- lab2_OpenMP/plot.py
+++ lab2_OpenMP/plot.py
@@ -5,57 +5,48 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 df1 = pd.read_csv('results_static.txt', ";")
-# df1.columns = ['loops', 'msg_size', 'time', 'nan']
+df1.columns = ['threads', 'size', 'time', 'nan']
 print(df1)
-# do 3 są ok (4 jest czasem za krótkie)
-# 5 dla większych wiadomości
-# def throughput(loops, df, fig, pl_name, gen_name):
-#     loops_1e4= df.loc[df['loops'] == loops]
-#     thr = []
-#     size = []
-#     for index, row in loops_1e4.iterrows():
-#         size.append(row['msg_size'])
-#         thr.append((2*loops*row['msg_size']*8)/row['time']/1024/1024)
 
 
-#     fig.add_trace(go.Scatter(
-#         x=size,
-#         y=thr,
-#         mode='markers',
-#         name=pl_name,
-#         marker_symbol='diamond',
-#         marker=dict(
-#             color=randint(1, 500),
-#             line_width=2,
-#             size=7,
-            
-#         ),
-#     ))
-#     fig.update_layout(
-#         title=gen_name,
-#         xaxis_title="Rozmiar wiadomości [B]",
-#         yaxis_title="Przepustowość [Mbits/s]"
-#     )
+def throughput(df, fig, limit, gen_name):
+    thr = [df.loc[df['threads'] == x] for x in range(1,5)]
 
-# def latency(df, msg):
-#     size_1 = df.loc[df['msg_size']==1]
-#     sum_time = size_1['time'].sum().sum()
-#     sum_loops = size_1['loops'].sum().sum()
+    x = {}
+    y = {}
+    for idx, results in enumerate(thr):
+        for index, row in results.iterrows():
+            if idx not in x or idx not in y:
+                x[idx] = []
+                y[idx] = []
+            if row['size'] < limit:
+                x[idx].append(row['size']) 
+                y[idx].append(row['time'])
 
-#     lat = sum_time/sum_loops
-#     print(msg+ " %.10f"%lat)
+    for id in range(1,5):
+        fig.add_trace(go.Scatter(
+            x=x[id-1][:-2],
+            y=y[id-1][:-2],
+            mode='markers',
+            name=str(id)+" thread",
+            marker_symbol='diamond',
+            marker=dict(
+                color=randint(1, 500),
+                line_width=2,
+                size=7,
+            ),
+        ))
+    fig.update_layout(
+        title=gen_name,
+        xaxis_title="Ilość elementów w tablicy tablicy",
+        yaxis_title="Czas trwania części równoległej [s]"
+    )
 
 
+fig = go.Figure()
+throughput(df1, fig, 300000000.0, "Czas trwania części równoległej programu z zależności od rozmiaru problemu")
+fig.show()
 
-# fig = go.Figure()
-# fig2 = go.Figure()
-# throughput(10000, df1, fig, "MPI_Send", "Przepustowość - vnode-03, 10000 powtórzeń")
-# throughput(10000, df2, fig2, "MPI_Send", "Przepustowość - vnode-08, vnode-09, 10000 powtórzeń")
-# throughput(10000, df3, fig, "MPI_Bsend", "Przepustowość - vnode-03, 10000 powtórzeń")
-# throughput(10000, df4, fig2, "MPI_Bsend", "Przepustowość - vnode-08, vnode-09, 10000 powtórzeń")
-# latency(df1, "Opóźnienie dla MPI_Send, vnode-03: ")
-# latency(df3, "Opóźnienie dla MPI_Bsend, vnode-03: ")
-# latency(df2, "Opóźnienie dla MPI_Send, vnode-08, vnode-09: ")
-# latency(df4, "Opóźnienie dla MPI_Bsend, vnode-08, vnode-09: ")
-# fig.show()
-# fig2.show()
+fig2 = go.Figure()
+throughput(df1, fig2, 15000000, "Czas trwania części równoległej programu z zależności od rozmiaru problemu")
+fig2.show()
