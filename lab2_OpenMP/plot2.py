f49a5e5a5b70eb9f15cb64178b582525e943c706
Natalia <natib13@gmail.com>
2022-04-07 01:11:38 +0200

    guided fix & plots

--- lab2_OpenMP/plot2.py
+++ lab2_OpenMP/plot2.py
@@ -43,7 +43,7 @@ def time_size(df, fig, limit, gen_name, sched_name):
 
 def time_schedule(sdict, schedules, gen_name):
     
-    for thr in range(0,4):
+    for thr in range(3,4):
         fig = go.Figure()
         x = {}
         y = {}
@@ -81,83 +81,52 @@ def time_schedule(sdict, schedules, gen_name):
         fig.show()
         print("one")
 
-def speedup(df, fig, limit, gen_name, sched_name):
-    thr = [df.loc[df['size'] == x] for x in range(1,5)]
 
-    x = {}
-    y = {}
-    for idx, results in enumerate(thr):
-        for index, row in results.iterrows():
-            if idx not in x or idx not in y:
-                x[idx] = []
-                y[idx] = []
-            if row['size'] < limit:
-                x[idx].append(row['size']) 
-                y[idx].append(row['time']/results.iloc[0]['time'])
+def speedup(fig, x1, yz1, yz2, yz3):
+    y1 = [yz1[0]/x for x in yz1]
+    y2 = []
+    y3 = []
 
-    for id in range(1,5):
-        fig.add_trace(go.Scatter(
-            x=x[id-1][:-2],
-            y=y[id-1][:-2],
-            mode='markers+lines',
-            name=str(id)+" thread",
-            marker_symbol='circle',
-            marker=dict(
-                # color=randint(1, 500),
-                line_width=1,
-                size=8,
-            ),
-            
-        ))
-    fig.update_xaxes(type="log")
-    fig.update_yaxes(type="log")
+    fig.add_trace(go.Scatter(
+        x=x1,
+        y=y1,
+        mode='markers',
+        name="zmierzone przyspieszenie (8*10^8 punktów)",
+        marker_symbol='diamond',
+        marker=dict(
+            color=randint(1, 500),
+            line_width=2,
+            size=7,
+        ),
+    ))
+    fig.add_trace(go.Scatter(
+        x=x1,
+        y=x1,
+        mode='lines',
+        name="idealne przyspieszenie",
+        # marker_symbol='diamond',
+        fillcolor='yellow',
+        marker=dict(
+            color=randint(1, 500),
+            line_width=2,
+            size=7,
+        ),
+    ))
+    
     fig.update_layout(
-        title=gen_name,
-        xaxis_title="Ilość elementów w tablicy tablicy",
+        title="Przyspieszenie programu",
+        xaxis_title="Ilość procesorów",
         yaxis_title="Przyspieszenie"
     )
-def speedup1(df, fig, limit, gen_name, sched_name):
-    thr = [df.loc[df['threads'] == x] for x in range(1,5)]
 
-    x = {}
-    y = {}
-    for idx, results in enumerate(thr):
-        for index, row in results.iterrows():
-            if idx not in x or idx not in y:
-                x[idx] = []
-                y[idx] = []
-            if row['size'] < limit:
-                x[idx].append(row['size']) 
-                y[idx].append(row['time']/results.iloc[0]['time'])
-
-    for id in range(1,5):
-        fig.add_trace(go.Scatter(
-            x=x[id-1][:-2],
-            y=y[id-1][:-2],
-            mode='markers+lines',
-            name=str(id)+" thread",
-            marker_symbol='circle',
-            marker=dict(
-                # color=randint(1, 500),
-                line_width=1,
-                size=8,
-            ),
-            
-        ))
-    fig.update_xaxes(type="log")
-    fig.update_yaxes(type="log")
-    fig.update_layout(
-        title=gen_name,
-        xaxis_title="Ilość elementów w tablicy tablicy",
-        yaxis_title="Przyspieszenie"
-    )
+    return x1, y1, y2, y3
 
 
 schedule = ['static', 'static,4', 'dynamic', 'dynamic,4', 'dynamic,50', 'guided,4', 'guided']
 sched_dict = {}
 for sched_name in schedule:
     df = pd.read_csv('results_'+sched_name.replace(',', '_')+'.txt', ";")
-    df.columns = ['threads', 'size', 'time', 'nan']
+    df.columns = ['time', ' ', ' ' ,  ' ',' ',' ',' ',' ',' ', 'threads', 'size', 'nan']
     sched_dict[sched_name] = [df.loc[df['threads'] == x] for x in range(1,5)]
 
 time_schedule(sched_dict, schedule, "Czas trwania części równoległej programu z zależności od rozmiaru problemu dla liczby wątków: ")
@@ -175,20 +144,10 @@ for sched_name in schedule:
     fig = go.Figure()
 
     df1 = pd.read_csv('results_'+sched_name.replace(',', '_')+'.txt', ";")
-    df1.columns = ['threads', 'size', 'time', 'nan']
+    df1.columns = ['time', ' ', ' ' ,  ' ',' ',' ',' ',' ',' ', 'threads', 'size', 'nan']
     print(df1)
 
     time_size(df1, fig, 3000000000.0, "Czas trwania części równoległej programu z zależności od rozmiaru problemu dla parametru schedule: "+sched_name)
     fig.show()
 
-# schedule = ['static', 'static,4', 'dynamic', 'dynamic,4', 'dynamic,50', 'guided,4', 'guided']
-# for sched_name in schedule:
-#     fig = go.Figure()
-
-#     df1 = pd.read_csv('results_'+sched_name.replace(',', '_')+'.txt', ";")
-#     df1.columns = ['threads', 'size', 'time', 'nan']
-#     print(df1)
-
-#     speedup(df1, fig, 3000000000.0, "Czas trwania części równoległej programu z zależności od rozmiaru problemu dla parametru schedule: ",sched_name)
-#     fig.show()
 
