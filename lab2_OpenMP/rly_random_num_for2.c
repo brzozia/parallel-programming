22759810c453b7d34027d4baec51c50d4fdb3077
brzozows <brzozows@vnode-03.dydaktyka.icsr.agh.edu.pl>
2022-04-06 23:51:47 +0200

    results

--- lab2_OpenMP/rly_random_num_for2.c
+++ lab2_OpenMP/rly_random_num_for2.c
@@ -10,24 +10,28 @@ int main(int argc, char ** argv){
         size = atoi(argv[1]);
     }
 
-    int *numbers = malloc(size*sizeof(int));
+    double *numbers = malloc(size*sizeof(double));
     if(numbers == NULL){
         printf("Cannot allocate so much memory.");
         return 1;
     }
 
     int i, thr;
-    int tim = time(NULL);
-    unsigned int seed;
+    // int tim = time(NULL);
+    // unsigned int seed;
+    unsigned short xi[3];
+    xi[0] = time(NULL);
+    xi[1] = time(NULL)+13;
+    xi[2] = time(NULL)+27;
 
     double start = omp_get_wtime(); 
-    #pragma omp parallel shared(numbers, tim, size, thr) private(i, seed)
+    #pragma omp parallel shared(numbers, size, xi) private(i)
     {
-        seed = omp_get_thread_num() + tim;
+        // seed = omp_get_thread_num() + tim;
 
-        #pragma omp for
+        #pragma omp for schedule(static)
        for(i=0; i<size; i++){
-            numbers[i] = rand_r(&seed);
+            numbers[i] = erand48(xi);
        }
 
       thr = omp_get_num_threads();
