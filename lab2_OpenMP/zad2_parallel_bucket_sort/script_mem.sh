22d5d0d0ac9396c86282aeb3ac79c73f49146bd7
Natalia Brzozowska <plgnbrzoza@login01.prometheus.cyfronet.pl>
2022-04-23 01:36:11 +0200

    results

--- lab2_OpenMP/zad2_parallel_bucket_sort/script_mem.sh
+++ lab2_OpenMP/zad2_parallel_bucket_sort/script_mem.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
-###SBATCH --nodes 1
-###SBATCH --ntasks 1
-###SBATCH --time=01:00:00
-###SBATCH --partition=plgrid
-###SBATCH --account=plgmpr22
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --time=01:00:00
+#SBATCH --partition=plgrid
+#SBATCH --account=plgmpr22
 
 python run_test_mem.py
