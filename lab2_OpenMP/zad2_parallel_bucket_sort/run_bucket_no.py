5628858f9b893d43d7aed79758825a8dbc009f85
Natalia Brzozowska <plgnbrzoza@login02.prometheus.cyfronet.pl>
2022-05-02 19:41:33 +0200

    a

--- lab2_OpenMP/zad2_parallel_bucket_sort/run_bucket_no.py
+++ lab2_OpenMP/zad2_parallel_bucket_sort/run_bucket_no.py
@@ -1,8 +1,8 @@
 import subprocess
 
-size = 70000000
+size = 500000
 
-result_file = "./res_buck_7_7.txt"
+result_file = "./res_buck_5_5_x10.txt"
 f = open(result_file, "a")
 f.write("min;max;size;buckets;mean_elements_in_bucket;threads;time_allocate_sync;time_deallocate;time_all;error;0;0;0;0;0;0;0;0;\n")
 f.close()
@@ -19,7 +19,7 @@ proces.wait()
     # f.write("threads; size; time;\n0; 0; 0;\n")
     # f.close()
     # for size in sizes:
-for x in range(0,5): 
+for x in range(0,10): 
     for bucket_size in range(1,100,1):
         proces = subprocess.Popen(["bash", "-c", "export OMP_NUM_THREADS=1; ./bucket " + str(size) + " 0 "+ str(bucket_size)+ " 1 " + result_file])
         proces.wait()
